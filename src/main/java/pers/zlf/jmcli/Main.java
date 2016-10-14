2ebd6d45c549d04f4b56775e86a13b865fc01475
zhoulifu <zlf369777684@gmail.com>
2016-10-14 12:49:09 +0800

    Refactor, remove class GameArgs

--- src/main/java/pers/zlf/jmcli/Main.java
+++ src/main/java/pers/zlf/jmcli/Main.java
@@ -13,8 +13,6 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.to2mbn.jmccc.auth.Authenticator;
-import org.to2mbn.jmccc.auth.OfflineAuthenticator;
-import org.to2mbn.jmccc.auth.yggdrasil.YggdrasilAuthenticator;
 import org.to2mbn.jmccc.launch.LaunchException;
 import org.to2mbn.jmccc.launch.Launcher;
 import org.to2mbn.jmccc.launch.LauncherBuilder;
@@ -25,14 +23,12 @@ import org.to2mbn.jmccc.option.LaunchOption;
 import org.to2mbn.jmccc.option.MinecraftDirectory;
 import org.to2mbn.jmccc.option.WindowSize;
 import org.to2mbn.jmccc.version.Library;
-import org.to2mbn.jmccc.version.Version;
-import org.to2mbn.jmccc.version.parsing.Versions;
 
+import pers.zlf.jmcli.util.AuthenticatorProxy;
 import pers.zlf.jmcli.util.LogCallback;
 import pers.zlf.jmcli.util.VersionComparator;
 
 public class Main {
-    private static String DEFAULT_PLAYER_NAME = "Hello Minecraft";
     private static String USAGE = "java -jar jmccc-cli.jar [OPTION]... /path/to/" +
                                   ".minecraft";
     private static Options OPTIONS;
@@ -54,90 +50,35 @@ public class Main {
                                       "specified, launch the latest version provided in" +
                                       " .minecraft/versions").build());
         OPTIONS.addOption(Option.builder(null).longOpt("min-heap").argName("size")
-                                .hasArg().desc("set initial Java heap size to SIZE Mb for " +
-                                      "launching minecraft").build());
+                                .desc("set initial Java heap size to SIZE Mb for " +
+                                      "launching minecraft").hasArg().build());
         OPTIONS.addOption(Option.builder(null).longOpt("max-heap").argName("size")
-                                .hasArg().desc("set maximum Java heap size to SIZE Mb for " +
-                                      "launching minecraft").build());
+                                .desc("set maximum Java heap size to SIZE Mb for " +
+                                      "launching minecraft").hasArg().build());
     }
 
-    public static void main(String[] args) throws Exception {
-
+    @SuppressWarnings("all")
+    public static void main(String[] args) {
         try {
-            GameArgs gameArgs = parse(args);
-            if (gameArgs != null) {
-                launch(gameArgs);
+            LaunchOption opt = parse(args);
+            try {
+                LAUNCHER.launch(opt);
+            } catch (LaunchException e) {
+                if (e instanceof MissingDependenciesException) {
+                    System.out.println("Missing Library: " + e.getMessage());
+                    downloadMissingJar(opt.getMinecraftDirectory(),
+                                       ((MissingDependenciesException) e)
+                                               .getMissingLibraries());
+                    System.out.println("Trying to relaunch minecraft");
+                    LAUNCHER.launch(opt);
+                } else {
+                    throw e;
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
             if (e instanceof ParseException) {
                 FORMATTER.printHelp(USAGE, Main.OPTIONS);
-            }
-        }
-    }
-
-    private static void launch(GameArgs args) throws LaunchException, InterruptedException {
-        MinecraftDirectory dir = new MinecraftDirectory(args.getDirectoryPath());
-
-        Authenticator auth;
-        if (args.isOfflineMode()) {
-            auth = new OfflineAuthenticator(
-                    args.getUsername() == null ? DEFAULT_PLAYER_NAME : args.getUsername());
-        } else {
-            auth = YggdrasilAuthenticator.password(args.getUsername(), args.getPassword());
-        }
-
-        String verStr;
-        String presentVer = args.getMcVer();
-        if (presentVer == null || presentVer.length() == 0) {
-            File versionsDir = dir.getVersions();
-            if (!versionsDir.exists() || !versionsDir.isDirectory()) {
-                throw new LaunchException("Versions directory not existed");
-            }
-
-            String[] vers = versionsDir.list();
-            Arrays.sort(vers, new VersionComparator());
-            verStr = vers[vers.length - 1];
-        } else {
-            verStr = presentVer;
-        }
-
-        Version version;
-        try {
-            version = Versions.resolveVersion(dir, verStr);
-        } catch (IOException e) {
-            throw new LaunchException("Parse version " + verStr + " failed", e);
-        }
-
-        if (version == null) {
-            throw new LaunchException("Version directory not existed: " + verStr);
-        }
-
-        LaunchOption opt = new LaunchOption(version, auth, dir);
-
-        if (args.getMinHeapSize() != null) {
-            opt.setMinMemory(args.getMinHeapSize());
-        }
-
-        if (args.getMaxHeapSize() != null) {
-            opt.setMaxMemory(args.getMaxHeapSize());
-        }
-
-        if (args.isFullScreen()) {
-            opt.setWindowSize(WindowSize.fullscreen());
-        }
-
-        try {
-            LAUNCHER.launch(opt);
-        } catch (LaunchException e) {
-            if (e instanceof MissingDependenciesException) {
-                System.out.println("Missing Library: " + e.getMessage());
-                downloadMissingJar(dir, ((MissingDependenciesException) e)
-                        .getMissingLibraries());
-                System.out.println("Trying to relaunch minecraft");
-                LAUNCHER.launch(opt);
-            } else {
-                throw e;
             }
         }
     }
@@ -161,41 +102,14 @@ public class Main {
         downloader.shutdown();
     }
 
-    private static GameArgs parse(String[] args) throws Exception {
+    private static LaunchOption parse(
+            String[] args) throws LaunchException, ParseException {
         DefaultParser parser = new DefaultParser();
-        GameArgs ags = new GameArgs();
         CommandLine cmd = parser.parse(Main.OPTIONS, args);
+
         if (cmd.hasOption("h")) {
             FORMATTER.printHelp(USAGE, OPTIONS);
-            return null;
-        }
-
-        if (cmd.hasOption('u')) {
-            ags.setUsername(cmd.getOptionValue('u'));
-        }
-
-        if (cmd.hasOption('p')) {
-            ags.setPassword(cmd.getOptionValue('p'));
-        }
-
-        if (cmd.hasOption("f")) {
-            ags.fullScreen();
-        }
-
-        if (cmd.hasOption("o")) {
-            ags.offlineMode();
-        }
-
-        if (cmd.hasOption("V")) {
-            ags.setMcVer(cmd.getOptionValue("V"));
-        }
-
-        if (cmd.hasOption("min-heap")) {
-            ags.setMinHeapSize(Integer.valueOf(cmd.getOptionValue("min-heap")));
-        }
-
-        if (cmd.hasOption("max-heap")) {
-            ags.setMaxHeapSize(Integer.valueOf(cmd.getOptionValue("max-heap")));
+            System.exit(0);
         }
 
         String[] path = cmd.getArgs();
@@ -203,8 +117,51 @@ public class Main {
             throw new ParseException("Missing Minecraft directory");
         }
 
-        ags.setDirectoryPath(path[0]);
+        MinecraftDirectory dir = new MinecraftDirectory(path[0]);
+        Authenticator auth = new AuthenticatorProxy(cmd.getOptionValue('u'),
+                                                    cmd.getOptionValue('p'),
+                                                    cmd.hasOption('o'));
+        String version = cmd.hasOption('V') ? cmd.getOptionValue('V') : latestVersion(
+                dir);
+        if (version == null || version.trim().length() == 0) {
+            throw new LaunchException("Version directory not existed");
+        }
 
-        return ags;
+        LaunchOption opt;
+        try {
+            opt = new LaunchOption(version, auth, dir);
+        } catch (IOException | IllegalArgumentException e) {
+            throw new LaunchException(e);
+        }
+
+        if (cmd.hasOption("f")) {
+            opt.setWindowSize(WindowSize.fullscreen());
+        }
+
+        if (cmd.hasOption("min-heap")) {
+            opt.setMinMemory(Integer.valueOf(cmd.getOptionValue("min-heap")));
+        }
+
+        if (cmd.hasOption("max-heap")) {
+            opt.setMaxMemory(Integer.valueOf(cmd.getOptionValue("max-heap")));
+        }
+
+        return opt;
+    }
+
+    @SuppressWarnings("all")
+    private static String latestVersion(MinecraftDirectory dir) throws LaunchException {
+        File versionsDir = dir.getVersions();
+        if (!versionsDir.exists() || !versionsDir.isDirectory()) {
+            throw new LaunchException("Versions directory not existed");
+        }
+
+        String[] vers = versionsDir.list();
+        if (vers.length == 0) {
+            return null;
+        }
+
+        Arrays.sort(vers, new VersionComparator());
+        return vers[vers.length - 1];
     }
 }
---
+++ src/main/java/pers/zlf/jmcli/util/AuthenticatorProxy.java
@@ -0,0 +1,45 @@
+package pers.zlf.jmcli.util;
+
+import org.to2mbn.jmccc.auth.AuthInfo;
+import org.to2mbn.jmccc.auth.AuthenticationException;
+import org.to2mbn.jmccc.auth.Authenticator;
+import org.to2mbn.jmccc.auth.OfflineAuthenticator;
+import org.to2mbn.jmccc.auth.yggdrasil.YggdrasilAuthenticator;
+
+public class AuthenticatorProxy implements Authenticator {
+    private static String DEFAULT_PLAYER_NAME = "Hello Minecraft";
+
+    private Authenticator authenticator;
+    private String username;
+    private String password;
+    private boolean offlineMode;
+
+    public AuthenticatorProxy(String username, String password, boolean offline) {
+        this.username = username;
+        this.password = password;
+        this.offlineMode = offline;
+    }
+
+    @Override
+    public AuthInfo auth() throws AuthenticationException {
+        return getAuthenticator().auth();
+    }
+
+    private Authenticator getAuthenticator() throws AuthenticationException {
+        if (authenticator == null) {
+            authenticator = create();
+        }
+
+        return authenticator;
+    }
+
+    private synchronized Authenticator create() throws AuthenticationException {
+        if (authenticator != null) {
+            return authenticator;
+        }
+
+        return offlineMode ? new OfflineAuthenticator(
+                (username == null ? DEFAULT_PLAYER_NAME : username))
+                           : YggdrasilAuthenticator.password(username, password);
+    }
+}
